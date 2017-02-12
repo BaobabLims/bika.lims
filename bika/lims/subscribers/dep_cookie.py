9a5444e423cedeedcefb87d0f7902ba7f15ba3aa
Ramon Bartl <rb@ridingbytes.com>
2017-02-12 19:19:35 +0100

    Fixed Attribute Error in Login Event handler
    Bail out if bika.lims is not installed yet.
    Refactored Code

--- bika/lims/subscribers/dep_cookie.py
+++ bika/lims/subscribers/dep_cookie.py
@@ -4,7 +4,9 @@
 #
 # Copyright 2011-2016 by it's authors.
 # Some rights reserved. See LICENSE.txt, AUTHORS.txt.
-from Products.CMFCore.utils import getToolByName
+
+from plone import api
+from bika.lims import logger
 
 
 def SetDepartmentCookies(event):
@@ -12,25 +14,47 @@ def SetDepartmentCookies(event):
     When user logs in for the first time, we are setting department filtering cookie values.
     """
 
-    # Getting request,response and username
-    request = event.object.REQUEST
-    response = request.RESPONSE
-    context = find_context(request)
-    mtool = getToolByName(context, 'portal_membership')
-    username = mtool.getAuthenticatedMember().getUserName()
-    dep_for_cookie = ''
+    # Fix for https://jira.bikalabs.com/browse/LIMS-2597
+    if not is_bika_installed():
+        logger.warn("Package 'bika.lims' is not installed, skipping event handler for IUserLoggedInEvent.")
+        return
 
-    if context.bika_setup.getAllowDepartmentFiltering():
+    # get the bika_setup object
+    portal = api.portal.get()
+    bika_setup = portal.get("bika_setup")
+
+    # just to be sure...
+    # This should go into the api.py module once it is in place
+    if bika_setup is None:
+        raise RuntimeError("bika_setup not found in this Bika LIMS installation")
+
+    # Getting request, response and username
+    request = api.env.getRequest()
+    response = request.RESPONSE
+
+    user = api.user.get_current()
+    username = user and user.getUserName() or None
+    portal_catalog = api.portal.get_tool("portal_catalog")
+
+    if bika_setup.getAllowDepartmentFiltering():
+        dep_for_cookie = ''
+
         if username == 'admin':
-            deps = context.portal_catalog(portal_type='Department', sort_on='sortable_title', sort_order='ascending',
-                                          inactive_state='active')
-            for dep in deps:
-                dep_for_cookie += dep.UID + ','
+            departments = portal_catalog(portal_type='Department',
+                                         sort_on='sortable_title',
+                                         sort_order='ascending',
+                                         inactive_state='active')
+
+            for department in departments:
+                dep_for_cookie += department.UID + ','
+
             response.setCookie('dep_filter_disabled', 'true', path='/', max_age=24 * 3600)
         else:
-            deps = context.portal_catalog(portal_type='LabContact',
-                                          getUsername=username)[0].getObject().getSortedDepartments()
-            dep_for_cookie = deps[0].UID() if len(deps) > 0 else ''
+            labcontact = portal_catalog(portal_type='LabContact', getUsername=username)
+            if labcontact:
+                departments = labcontact[0].getObject().getSortedDepartments()
+                dep_for_cookie = departments[0].UID() if len(departments) > 0 else ''
+
         response.setCookie('filter_by_department_info', dep_for_cookie, path='/', max_age=24 * 3600)
     else:
         response.setCookie('filter_by_department_info', None, path='/', max_age=0)
@@ -41,18 +65,21 @@ def ClearDepartmentCookies(event):
     """Logout event handler.
     When user explicitly logs out from the Logout menu, clean department filtering related cookies.
     """
-    request = event.object.REQUEST
+
+    if not is_bika_installed():
+        logger.warn("Package 'bika.lims' is not installed, skipping event handler for IUserLoggedOutEvent.")
+        return
+
+    request = api.env.getRequest()
     response = request.RESPONSE
+
     # Voiding our special cookie on logout
     response.setCookie('filter_by_department_info', None, path='/', max_age=0)
     response.setCookie('dep_filter_disabled', None, path='/', max_age=0)
 
 
-def find_context(request):
-    """Find the context from the request
+def is_bika_installed():
+    """Check if Bika LIMS is installed in the Portal
     """
-    published = request.get('PUBLISHED', None)
-    context = getattr(published, '__parent__', None)
-    if context is None:
-        context = request.PARENTS[0]
-    return context
+    qi = api.portal.get_tool("portal_quickinstaller")
+    return qi.isProductInstalled("bika.lims")
