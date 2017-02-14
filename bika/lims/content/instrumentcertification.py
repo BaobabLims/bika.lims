af68dcf1a8f4e33ff5feabc377876895f35f742b
Ramon Bartl <rb@ridingbytes.com>
2017-02-14 15:03:01 +0100

    PEP8

--- bika/lims/content/instrumentcertification.py
+++ bika/lims/content/instrumentcertification.py
@@ -5,29 +5,54 @@
 # Copyright 2011-2017 by it's authors.
 # Some rights reserved. See LICENSE.txt, AUTHORS.txt.
 
+from DateTime import DateTime
 from AccessControl import ClassSecurityInfo
-from Products.ATContentTypes.content import schemata
-from Products.Archetypes import atapi
-from Products.Archetypes.public import *
+
+from Products.Archetypes.atapi import BaseFolder
+from Products.Archetypes.atapi import DisplayList
+from Products.Archetypes.atapi import registerType
+
 from zope.interface import implements
-from bika.lims import bikaMessageFactory as _
-from bika.lims.utils import t
-from bika.lims.browser.widgets import DateTimeWidget, ReferenceWidget
+from plone import api as ploneapi
+
+# Schema and Fields
+from Products.Archetypes.atapi import Schema
+from Products.Archetypes.atapi import ReferenceField
+from Products.Archetypes.atapi import ComputedField
+from Products.Archetypes.atapi import DateTimeField
+from Products.Archetypes.atapi import StringField
+from Products.Archetypes.atapi import TextField
+from Products.Archetypes.atapi import FileField
+from Products.Archetypes.atapi import BooleanField
+
+# Widgets
+from Products.Archetypes.atapi import ComputedWidget
+from Products.Archetypes.atapi import StringWidget
+from Products.Archetypes.atapi import TextAreaWidget
+from Products.Archetypes.atapi import FileWidget
+from Products.Archetypes.atapi import BooleanWidget
+from bika.lims.browser.widgets import DateTimeWidget
+from bika.lims.browser.widgets import ReferenceWidget
+
+# bika.lims imports
 from bika.lims.config import PROJECTNAME
+from bika.lims import bikaMessageFactory as _
 from bika.lims.content.bikaschema import BikaSchema
-from Products.CMFCore import permissions
 from bika.lims.interfaces import IInstrumentCertification
+
 
 schema = BikaSchema.copy() + Schema((
 
-    StringField('TaskID',
-        widget = StringWidget(
+    StringField(
+        'TaskID',
+        widget=StringWidget(
             label=_("Task ID"),
             description=_("The instrument's ID in the lab's asset register"),
         )
     ),
 
-    ReferenceField('Instrument',
+    ReferenceField(
+        'Instrument',
         allowed_types=('Instrument',),
         relationship='InstrumentCertificationInstrument',
         widget=StringWidget(
@@ -35,8 +60,9 @@ schema = BikaSchema.copy() + Schema((
         )
     ),
 
-    ComputedField('InstrumentUID',
-        expression = 'context.getInstrument() and context.getInstrument().UID() or None',
+    ComputedField(
+        'InstrumentUID',
+        expression='context.getInstrument() and context.getInstrument().UID() or None',
         widget=ComputedWidget(
             visible=False,
         ),
@@ -44,7 +70,8 @@ schema = BikaSchema.copy() + Schema((
 
     # Set the Certificate as Internal
     # When selected, the 'Agency' field is hidden
-    BooleanField('Internal',
+    BooleanField(
+        'Internal',
         default=False,
         widget=BooleanWidget(
             label=_("Internal Certificate"),
@@ -52,41 +79,46 @@ schema = BikaSchema.copy() + Schema((
         )
     ),
 
-    StringField('Agency',
-        widget = StringWidget(
+    StringField(
+        'Agency',
+        widget=StringWidget(
             label=_("Agency"),
             description=_("Organization responsible of granting the calibration certificate")
         ),
     ),
 
-    DateTimeField('Date',
-        widget = DateTimeWidget(
+    DateTimeField(
+        'Date',
+        widget=DateTimeWidget(
             label=_("Date"),
             description=_("Date when the calibration certificate was granted"),
         ),
     ),
 
-    DateTimeField('ValidFrom',
-        with_time = 1,
-        with_date = 1,
-        required = 1,
-        widget = DateTimeWidget(
+    DateTimeField(
+        'ValidFrom',
+        with_time=1,
+        with_date=1,
+        required=1,
+        widget=DateTimeWidget(
             label=_("From"),
             description=_("Date from which the calibration certificate is valid"),
         ),
     ),
 
-    DateTimeField('ValidTo',
-        with_time = 1,
-        with_date = 1,
-        required = 1,
-        widget = DateTimeWidget(
+    DateTimeField(
+        'ValidTo',
+        with_time=1,
+        with_date=1,
+        required=1,
+        widget=DateTimeWidget(
             label=_("To"),
             description=_("Date until the certificate is valid"),
         ),
     ),
 
-    ReferenceField('Preparator',
+    ReferenceField(
+        'Preparator',
         vocabulary='getLabContacts',
         allowed_types=('LabContact',),
         relationship='LabContactInstrumentCertificatePreparator',
@@ -97,14 +129,16 @@ schema = BikaSchema.copy() + Schema((
             size=30,
             base_query={'inactive_state': 'active'},
             showOn=True,
-            colModel=[{'columnName': 'UID', 'hidden': True},
-                      {'columnName': 'JobTitle', 'width': '20', 'label': _('Job Title')},
-                      {'columnName': 'Title', 'width': '80', 'label': _('Name')}
-                     ],
+            colModel=[
+                {'columnName': 'UID', 'hidden': True},
+                {'columnName': 'JobTitle', 'width': '20', 'label': _('Job Title')},
+                {'columnName': 'Title', 'width': '80', 'label': _('Name')}
+            ],
         ),
     ),
 
-    ReferenceField('Validator',
+    ReferenceField(
+        'Validator',
         vocabulary='getLabContacts',
         allowed_types=('LabContact',),
         relationship='LabContactInstrumentCertificateValidator',
@@ -115,24 +149,27 @@ schema = BikaSchema.copy() + Schema((
             size=30,
             base_query={'inactive_state': 'active'},
             showOn=True,
-            colModel=[{'columnName': 'UID', 'hidden': True},
-                      {'columnName': 'JobTitle', 'width': '20', 'label': _('Job Title')},
-                      {'columnName': 'Title', 'width': '80', 'label': _('Name')}
-                     ],
+            colModel=[
+                {'columnName': 'UID', 'hidden': True},
+                {'columnName': 'JobTitle', 'width': '20', 'label': _('Job Title')},
+                {'columnName': 'Title', 'width': '80', 'label': _('Name')}
+            ],
         ),
     ),
 
-    FileField('Document',
-        widget = FileWidget(
+    FileField(
+        'Document',
+        widget=FileWidget(
             label=_("Report upload"),
             description=_("Load the certificate document here"),
         )
     ),
 
-    TextField('Remarks',
+    TextField(
+        'Remarks',
         searchable=True,
         default_content_type='text/x-web-intelligent',
-        allowable_content_types = ('text/plain', ),
+        allowable_content_types=('text/plain', ),
         default_output_type="text/plain",
         mode="rw",
         widget=TextAreaWidget(
@@ -144,21 +181,22 @@ schema = BikaSchema.copy() + Schema((
 
 ))
 
-schema['title'].widget.label=_("Certificate Code")
+schema['title'].widget.label = _("Certificate Code")
+
 
 class InstrumentCertification(BaseFolder):
     implements(IInstrumentCertification)
     security = ClassSecurityInfo()
     schema = schema
     displayContentsTab = False
-
     _at_rename_after_creation = True
+
     def _renameAfterCreation(self, check_auto_id=False):
         from bika.lims.idserver import renameAfterCreation
         renameAfterCreation(self)
 
     def getLabContacts(self):
-        bsc = getToolByName(self, 'bika_setup_catalog')
+        bsc = ploneapi.portal.get_tool(self, 'bika_setup_catalog')
         # fallback - all Lab Contacts
         pairs = []
         for contact in bsc(portal_type='LabContact',
