cc86f556466ec9777546f01a0c019ed6e20df159
Alexander <alex@bikalabs.com>
2017-04-25 14:49:43 +0200

    fix ar copy button on dropdown/select (#2018)

--- bika/lims/browser/js/bika.lims.analysisrequest.add_by_col.js
+++ bika/lims/browser/js/bika.lims.analysisrequest.add_by_col.js
@@ -697,10 +697,10 @@ function AnalysisRequestAddByCol() {
             }
             // select element
             else if ($(td1).find('select').length > 0) {
-                var input = $(td1).find('.rejectionwidget-input-other').val();
+                var input = $(td1).find('select').val();
                 for (var arnum = 1; arnum < nr_ars; arnum++) {
                     td = $(tr).find('td[arnum="' + arnum + '"]')[0];
-                    e = $(td).find('.rejectionwidget-input-other')[0];
+                    e = $(td).find('select')[0];
                     $(e).val(input);
                     $(e).trigger('copy');
                 }
